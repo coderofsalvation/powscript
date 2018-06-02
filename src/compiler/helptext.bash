47bcd5af183354c1fa2639fc0b0ea371a5308029
Fabio Cardeal <ficarde@gmail.com>
2018-06-01 23:31:29 -0300

    More help text

--- src/compiler/helptext.bash
+++ src/compiler/helptext.bash
@@ -3,20 +3,25 @@ powscript:help() {
   usage: powscript [options...] input_files...
 
   options:
-    -h|--help           Display this message and exit.
+    -h|--help             Display this message and exit.
 
-    -i|--interactive    Launch interactive mode.
-                        Default if no input files are given.
+    -i|--interactive      Launch interactive mode.
+                          Default if no input files are given.
 
-    -c|--compile        Force compilation mode.
+    -c|--compile          Force compilation mode.
 
-    -o|--output $file   Compile all input files into $file.
-                        If this option is not given, the compilation
-                        is sent to the standard output.
+    -o|--output $file     Compile all input files into $file.
+                          If this option is not given, the compilation
+                          is sent to the standard output.
 
-    -d|--debug          Debug mode for developers.
+    -e|--evaluate $expr   Evaluate a powscript expression.
 
-    --to sh|bash        Select target language for compilation.
+    --no-std              Don'"'"'t import the standard library,
+                          decreasing startup time.
+
+    -d|--debug            Debug mode for developers.
+
+    --to sh|bash          Select target language for compilation.
 
   '
 }
